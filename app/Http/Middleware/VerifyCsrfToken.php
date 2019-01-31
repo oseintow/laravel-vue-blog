82a95411f94b1b2c2585b1631692072bc873ad92
michael ntow <oseintow@gmail.com>
2019-01-31 10:09:17 +0000

    ch(): added socialite config to services

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,6 +19,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        //
+        '/sociallogin/google',
+        '/sociallogin/facebook'
     ];
 }
--- config/services.php
+++ config/services.php
@@ -40,4 +40,16 @@ return [
         ],
     ],
 
+    'facebook' => [
+        'client_id'     => env('FACEBOOK_ID'),
+        'client_secret' => env('FACEBOOK_SECRET'),
+        'redirect'      => env('FACEBOOK_URL'),
+    ],
+
+    'google' => [
+        'client_id'     => env('GOOGLE_ID'),
+        'client_secret' => env('GOOGLE_SECRET'),
+        'redirect'      => env('GOOGLE_URL'),
+    ],
+
 ];

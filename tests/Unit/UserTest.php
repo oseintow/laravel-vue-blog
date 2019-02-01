11dc5981fbb6e7bd12aea3246939b175a927422e
michael ntow <oseintow@gmail.com>
2019-02-01 12:06:51 +0000

    ch(): added UserTest stub

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}

ed20f01b6ff7dd741c33f02b9e032e15e51fecc5
michael ntow <oseintow@gmail.com>
2019-02-05 21:09:23 +0000

    en(): refactored new blog

--- resources/js/components/blog/BlogPostEditor.vue
+++ resources/js/components/blog/BlogPostEditor.vue
@@ -1,35 +1,9 @@
 <template>
     <div class="blog-post-editor">
-        <div class="post-inputs">
-            <div class="title-input">
-                <error>Title should be more than 10 characters</error>
-                <Input v-model="title" placeholder="Post title"/>
-            </div>
-            <div class="cat-input">
-                <error>Post must have a category</error>
-                <Select placeholder="Select a  Category"
-                        v-model="category"
-                        v-if="postCategories.length">
-                    <option v-for="(cat, index) in postCategories" :key="index" :value="cat.id">{{cat.label}}</option>
-                </Select>
-            </div>
-            <div class="image-input">
-                <error>Post must have an image</error>
-                <div class="inner-image-input">
-                    <img :src="image_url" alt="" v-if="image_url"/>
-                    <Button type="primary" class="flex-center" icon="ios-camera-outline">Post Image</Button>
-                </div>
-            </div>
-        </div>
         <quill-editor v-model="content"
             ref="myQuillEditor"
             :options="editorOption">
         </quill-editor>
-        <div class="post-actions-row">
-            <Button class="btn-delete" @click="del">Delete</Button>
-            <Button type="primary" @click="save">Save</Button>
-            <Button type="primary" @click="publish">Publish</Button>
-        </div>
     </div>
 </template>
 
@@ -39,23 +13,14 @@
     import 'quill/dist/quill.snow.css'
 
     import { quillEditor } from 'vue-quill-editor'
-    import { Button, Input, Select } from 'iview'
-    import Error from '@/components/Error'
 
     export default {
         name: "BlogPostEditor",
         components: {
-            Button,
-            Input,
-            Select,
-            quillEditor,
-            Error
+            quillEditor
         },
         data() {
             return {
-                title: '',
-                category:'',
-                image_url: '',
                 content: '',
                 editorOption: {
                     debug: 'info',
@@ -63,35 +28,13 @@
                     readOnly: true,
                     theme: 'snow'
                 },
-                delta: undefined,
-                postCategories: [
-                    {
-                        id: 1,
-                        label: 'Nodejs',
-                        img: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/full/nodejslogo.png'
-                    },
-                    {
-                        id: 2,
-                        label: 'Javascript',
-                        img: 'https://cdn-images-1.medium.com/max/1600/1*zWhOGf_PgX0nRTLZLmFpGg.png'
-                    }
-                ]
+                delta: undefined
             }
         },
         watch: {
             content() {
-                this.delta = this.$refs.myQuillEditor.quill.getContents()
-            }
-        },
-        methods: {
-            del() {
-
-            },
-            save() {
-
-            },
-            publish() {
-
+                // this.delta = this.$refs.myQuillEditor.quill.getContents()
+                this.$emit('delta', this.$refs.myQuillEditor.quill.getContents())
             }
         }
     }
@@ -100,17 +43,5 @@
 <style>
     .ql-editor{
         height: 72vh
-    }
-    .inner-image-input{
-        display: flex;
-        align-items: center;
-    }
-    .flex-center{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .ivu-icon-ios-camera-outline{
-        font-size: 16px
     }
 </style>
--- resources/js/pages/NewBlog.vue
+++ resources/js/pages/NewBlog.vue
@@ -1,27 +1,104 @@
 <template>
     <div>
         <h3>Medium Editor</h3>
-        <blog-post-editor></blog-post-editor>
+        <div class="post-inputs">
+            <div class="title-input">
+                <error>Title should be more than 10 characters</error>
+                <Input v-model="title" placeholder="Post title"/>
+            </div>
+            <div class="cat-input">
+                <error>Post must have a category</error>
+                <Select placeholder="Select a Category"
+                        v-model="category"
+                        v-if="postCategories.length">
+                    <Option v-for="(cat, index) in postCategories" :key="index" :value="cat.id">
+                        {{cat.label}}
+                    </Option>
+                </Select>
+            </div>
+            <div class="image-input">
+                <error>Post must have an image</error>
+                <div class="inner-image-input">
+                    <img :src="image_url" alt="" v-if="image_url"/>
+                    <Button @click="showImageModel = true" type="primary" class="flex-center" icon="ios-camera-outline">Post Image</Button>
+                </div>
+            </div>
+        </div>
+        <blog-post-editor @delta="delta"></blog-post-editor>
+        <div class="post-actions-row">
+            <Button class="btn-delete" @click="del">Delete</Button>
+            <Button type="primary" @click="save">Save</Button>
+            <Button type="primary" @click="publish">Publish</Button>
+        </div>
     </div>
 </template>
 
 <script>
     import BlogPostEditor from '@/components/blog/BlogPostEditor'
+    import { Button, Input, Select } from 'iview'
+    import Error from '@/components/Error'
 
     export default {
         name: "NewBlog",
         components: {
-            BlogPostEditor
+            BlogPostEditor,
+            Button,
+            Input,
+            Select,
+            Error
         },
         data() {
             return {
+                title: '',
+                category:'',
+                image_url: '',
+                showImageModel: false,
+                postCategories: [
+                    {
+                        id: 1,
+                        label: 'Nodejs',
+                        img: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/full/nodejslogo.png'
+                    },
+                    {
+                        id: 2,
+                        label: 'Javascript',
+                        img: 'https://cdn-images-1.medium.com/max/1600/1*zWhOGf_PgX0nRTLZLmFpGg.png'
+                    }
+                ]
             }
         },
         methods: {
+            delta(value) {
+                console.log(value)
+            },
+            del() {
 
+            },
+            save() {
+
+            },
+            publish() {
+
+            }
         }
     }
 </script>
 
-<style scoped>
+<style>
+    .inner-image-input{
+        display: flex;
+        align-items: center;
+    }
+    .flex-center{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    .post-actions-row {
+        display: flex;
+        justify-content: space-between;
+    }
+    .ivu-icon-ios-camera-outline{
+        font-size: 16px
+    }
 </style>

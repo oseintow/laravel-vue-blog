f5efdc97d71b78ade017e5aebb647e2a70c9097c
michael ntow <oseintow@gmail.com>
2019-02-05 12:44:54 +0000

    feat(): added error component

---
+++ resources/js/components/Error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="err-text">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Error"
+    }
+</script>
+
+<style scoped>
+    .err-test {
+        color: red
+    }
+</style>
--- resources/js/components/blog/BlogPostEditor.vue
+++ resources/js/components/blog/BlogPostEditor.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="blog-post-editor">
+        <div class="post-inputs">
+            <div class="title-input">
+                <Input/>
+                <error>Title should be more than 10 characters</error>
+            </div>
+            <div class="cat-input">
+                <error>Post must have a category</error>
+            </div>
+            <div class="image-input">
+
+            </div>
+        </div>
         <quill-editor v-model="content"
             ref="myQuillEditor"
             :options="editorOption">
@@ -14,14 +26,16 @@
 
     import { quillEditor } from 'vue-quill-editor'
     import { Button, Input, Select } from 'iview'
+    import Error from '@/components/Error'
 
     export default {
         name: "BlogPostEditor",
         components: {
             Button,
-                Input,
-                Select,
-                quillEditor
+            Input,
+            Select,
+            quillEditor,
+            Error
         },
         data() {
             return {
@@ -32,7 +46,19 @@
                     readOnly: true,
                     theme: 'snow'
                 },
-                delta: undefined
+                delta: undefined,
+                postCategories: [
+                    {
+                        id: 1,
+                        label: 'Nodejs',
+                        img: 'https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/256/full/nodejslogo.png'
+                    },
+                    {
+                        id: 2,
+                        label: 'Javascript',
+                        img: 'https://cdn-images-1.medium.com/max/1600/1*zWhOGf_PgX0nRTLZLmFpGg.png'
+                    }
+                ]
             }
         },
         watch: {

655d485f5eb9b61bed08f31d6c73f58b9792041a
michael ntow <oseintow@gmail.com>
2019-02-16 02:52:27 +0000

    ch(test): getting all comment for a blog

--- tests/Unit/CommentTest.php
+++ tests/Unit/CommentTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Blog;
 use App\Comment;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -17,5 +18,24 @@ class CommentTest extends TestCase
         $comment = create(Comment::class);
 
         $this->assertInstanceOf(Blog::class, $comment->blog);
+    }
+
+    /** @test */
+    public function a_comment_is_owned_by_a_user()
+    {
+        $comment = create(Comment::class);
+
+        $this->assertInstanceOf(User::class, $comment->owner);
+    }
+
+    /** @test */
+    public function it_can_get_all_comments_that_belongs_to_a_blog()
+    {
+        $blog = create(Blog::class);
+
+        create(Comment::class, ['blog_id' => $blog->id], 2);
+        create(Comment::Class, [], 5);
+
+        $this->assertEquals(2, Comment::onBlog($blog->slug)->count());
     }
 }

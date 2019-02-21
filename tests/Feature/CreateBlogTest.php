e5471e327e15e5840216fb5f81ac41dde438620d
michael ntow <oseintow@gmail.com>
2019-02-21 16:57:47 +0000

    ch(): moved authentication test down

--- tests/Feature/CreateBlogTest.php
+++ tests/Feature/CreateBlogTest.php
@@ -15,48 +15,6 @@ class CreateBlogTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function unauthernticated_users_can_not_create_blog()
-    {
-        $response = $this->withExceptionHandling()
-            ->json('POST', 'v1/blogs', []);
-
-        $response->assertStatus(401);
-    }
-
-    /** @test */
-    public function authernticated_users_can_create_blog()
-    {
-        $this->signIn();
-        $blog = make(Blog::class)->makeHidden('user_id')->toArray();
-        $blog['body'] = json_encode(["foo" => "bar"]);
-
-        $response = $this->withExceptionHandling()
-            ->json('POST', 'v1/blogs', $blog);
-
-        $this->assertDatabaseHas('blogs', ['title' => $blog['title']]);
-        $response->assertStatus(200);
-    }
-
-    /** @test */
-    public function authenticated_users_can_upload_cover_images_when_creating_blog_post()
-    {
-        $this->signIn();
-        Storage::fake('local');
-
-        $blog = make(Blog::class, [
-            'cover_image' => $coverImage = UploadedFile::fake()->image('random.jpg')
-        ])->makeHidden('user_id')->toArray();
-
-        $blog['body'] = json_encode(["foo" => "bar"]);
-
-        $this->withExceptionHandling()
-            ->json('POST', 'v1/blogs', $blog);
-
-        $this->assertDatabaseHas('blogs', ['cover_image_url' => '/images/cover_images/' . $coverImage->hashName()]);
-        Storage::disk('local')->assertExists('cover_images/' . $coverImage->hashName());
-    }
-
-    /** @test */
     public function a_blog_requires_a_title()
     {
         $this->validatePostRequest(['title' => null])
@@ -150,5 +108,47 @@ class CreateBlogTest extends TestCase
                 'publish' => true
             ],$body))
             ->assertStatus(422);
+    }
+
+    /** @test */
+    public function unauthernticated_users_can_not_create_blog()
+    {
+        $response = $this->withExceptionHandling()
+            ->json('POST', 'v1/blogs', []);
+
+        $response->assertStatus(401);
+    }
+
+    /** @test */
+    public function authernticated_users_can_create_blog()
+    {
+        $this->signIn();
+        $blog = make(Blog::class)->makeHidden('user_id')->toArray();
+        $blog['body'] = json_encode(["foo" => "bar"]);
+
+        $response = $this->withExceptionHandling()
+            ->json('POST', 'v1/blogs', $blog);
+
+        $this->assertDatabaseHas('blogs', ['title' => $blog['title']]);
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function authenticated_users_can_upload_cover_images_when_creating_blog_post()
+    {
+        $this->signIn();
+        Storage::fake('local');
+
+        $blog = make(Blog::class, [
+            'cover_image' => $coverImage = UploadedFile::fake()->image('random.jpg')
+        ])->makeHidden('user_id')->toArray();
+
+        $blog['body'] = json_encode(["foo" => "bar"]);
+
+        $this->withExceptionHandling()
+            ->json('POST', 'v1/blogs', $blog);
+
+        $this->assertDatabaseHas('blogs', ['cover_image_url' => '/images/cover_images/' . $coverImage->hashName()]);
+        Storage::disk('local')->assertExists('cover_images/' . $coverImage->hashName());
     }
 }

ab777428ed264fe81f9270359bab441b4380a57b
michael ntow <oseintow@gmail.com>
2019-02-22 03:04:03 +0000

    en(): added media query to search box

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <div class="row">
+        <div class="row xs-column-reverse">
             <div class="col-md-8">
                 <div class="mr-4">
                     <blogs></blogs>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="row">
-                    <ul class="list-group d-flex" style="width: 100%">
+                <div class="row mb-4">
+                    <ul class="list-group searchBox">
                         <li class="list-group-item"><h6>Search</h6></li>
                         <li class="list-group-item">
                             <search placeholder="search for blog"></search>
@@ -40,5 +40,17 @@
 </script>
 
 <style scoped>
-
+    .searchBox {
+        width: 100%;
+    }
+    @media only screen and (max-width: 768px) {
+        .xs-column-reverse {
+            display: flex;
+            flex-direction: column-reverse;
+        }
+        .searchBox {
+            margin-left: 5%;
+            margin-right: 5%;
+        }
+    }
 </style>

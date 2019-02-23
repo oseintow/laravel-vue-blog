3f24a2d0e745467aef501d138f55da362d29952c
michael ntow <oseintow@gmail.com>
2019-02-23 10:46:37 +0000

    gs

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -2,40 +2,13 @@
     <div>
         <form novalidate>
             <div class="form-group">
-                <label for="name">Name:</label>
-                <input type="text"
-                       v-model="user.name"
-                       name="name"
-                       class="form-control"
-                       id="name"
-                       placeholder=""
-                       v-validate="'required|min:3|max:100'">
-                <error v-show="errors.has('name')">{{ errors.first('name') }}</error>
-            </div>
-
-            <div class="form-group">
-                <label for="nickname">Nickname:</label>
-                <input type="text"
-                       v-model="user.nickname"
-                       name="nickname"
-                       class="form-control"
-                       id="nickname"
-                       placeholder=""
-                       v-validate="'required|min:3|max:50'">
-                <error v-show="errors.has('nickname')">{{ errors.first('nickname') }}</error>
-            </div>
-
-            <div class="form-group">
                 <label for="email">Email:</label>
                 <input type="text"
                        v-model="user.email"
                        name="email"
                        class="form-control"
                        id="email"
-                       placeholder=""
-                       data-vv-as="email"
-                       v-validate="'required|email'">
-                <error v-show="errors.has('email')">{{ errors.first('email') }}</error>
+                       placeholder="">
             </div>
 
 
@@ -47,22 +20,7 @@
                        class="form-control"
                        id="password"
                        ref="password"
-                       placeholder=""
-                       v-validate="'required|min:6|max:50'">
-                <error v-show="errors.has('password')">{{ errors.first('password') }}</error>
-            </div>
-
-            <div class="form-group">
-                <label for="password_confirmation">Confirm Password:</label>
-                <input type="password"
-                       v-model="user.confirm_password"
-                       name="password_confirmation"
-                       class="form-control"
-                       id="password_confirmation"
-                       placeholder=""
-                       data-vv-as="password"
-                       v-validate="'required|confirmed:password'">
-                <error v-show="errors.has('password_confirmation')">{{ errors.first('password_confirmation') }}</error>
+                       placeholder="">
             </div>
 
             <button type="submit" class="btn btn-primary" @click="submit">Submit</button>
--- resources/js/components/SignIn.vue
+++ resources/js/components/SignIn.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
         <b-modal v-model="show" id="authentication-modal" hide-footer size="lg" centered title="">
-            <button @click="authenticate('facebook')" class="facebook-signin">Facebook</button>
-            <button @click="authenticate('google')" class="google-signin">Google</button>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="col-md-6">
+                        <login></login>
+                    </div>
+                    <div class="col-md-6">
+                        <button @click="authenticate('facebook')" class="facebook-signin">Facebook</button>
+                        <button @click="authenticate('google')" class="google-signin">Google</button>
+                    </div>
+                </div>
+            </div>
         </b-modal>
     </div>
 </template>
 
 <script>
 import AuthenticationMixin from '@/mixins/AuthenticationMixin'
+import Login from '@/components/Login'
 
 export default {
     name: "SignIn",
     mixins: [AuthenticationMixin],
+    components: {
+        Login
+    },
     created() {
         this.$eventBus.$on('sign-in', () => this.showSignIn())
     },

755db7b5dfdf929e17fb11de795555ab006b6861
michael ntow <oseintow@gmail.com>
2019-02-23 08:28:48 +0000

    test(refactor): register user

--- tests/Feature/RegisterUserTest.php
+++ tests/Feature/RegisterUserTest.php
@@ -9,17 +9,11 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class RegisterUserTest extends TestCase
 {
     use RefreshDatabase;
-
+    
     /** @test */
     public function a_user_can_create_an_account()
     {
-        $user = factory(User::class)
-            ->states('nickname')
-            ->make()
-            ->makeVisible(['password']);
-
-        $this->installPassport()
-            ->json('POST', '/v1/register', $user->toArray())
+        $this->registerUser()
             ->assertStatus(200)
             ->assertJsonStructure([
                 'token',
@@ -33,5 +27,22 @@ class RegisterUserTest extends TestCase
                     'updated_at'
                 ]
             ]);
+    }
+
+    /** @test */
+    public function a_users_name_is_required()
+    {
+
+    }
+
+    protected function registerUser(array $body = [])
+    {
+        $user = factory(User::class)
+            ->states('nickname')
+            ->make()
+            ->makeVisible(['password']);
+
+        return $this->installPassport()
+            ->json('POST', '/v1/register', array_merge($user->toArray(), $body));
     }
 }

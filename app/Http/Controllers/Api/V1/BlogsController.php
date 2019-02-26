c836999fb3459a7fc576056936884fedf9929b97
michael ntow <oseintow@gmail.com>
2019-02-26 02:05:54 +0000

    ch(): added delete blog test

--- app/Http/Controllers/Api/V1/BlogsController.php
+++ app/Http/Controllers/Api/V1/BlogsController.php
@@ -17,7 +17,7 @@ class BlogsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api')->only(['store']);
+        $this->middleware('auth:api')->only(['store', 'update', 'destroy']);
     }
 
     /**
---
+++ tests/Feature/DeleteBlogTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Blog;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DeleteBlogTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $user;
+    protected $blog;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = create(User::class);
+        $this->blog = create(Blog::class, ['user_id' => $this->user->id]);
+    }
+
+    /** @test */
+    public function an_authenticated_user_can_delete_blog()
+    {
+        $this->assertDatabaseHas('blogs', ['id' => $this->blog->id]);
+
+        $this->signIn($this->user)
+            ->deleteBlog()
+            ->assertStatus(200);
+
+        $this->assertDatabaseMissing('blogs', ['id' => $this->blog->id]);
+    }
+
+    /** @test */
+    public function a_user_can_not_update_other_users_blog()
+    {
+        $this->signIn()
+            ->deleteBlog()
+            ->assertStatus(403);
+    }
+
+    protected function deleteBlog()
+    {
+        $slug = $this->blog->slug;
+
+        return $this->withExceptionHandling()
+            ->json('DELETE', "/v1/blogs/{$slug}");
+    }
+}

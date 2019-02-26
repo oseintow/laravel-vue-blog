4f507cf78cd9f59e2c9981ad26d1d7ca53c32629
michael ntow <oseintow@gmail.com>
2019-02-26 01:59:41 +0000

    Removed unneeded import

--- tests/Feature/DeleteCommentTest.php
+++ tests/Feature/DeleteCommentTest.php
@@ -5,7 +5,6 @@ namespace Tests\Feature;
 use App\Comment;
 use App\User;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DeleteCommentTest extends TestCase

b07dc003446d798f306cf642471e7fed5373e807
michael ntow <oseintow@gmail.com>
2019-03-04 00:07:14 +0000

    ch(): getting getters

---
+++ resources/js/api/__mocks__/blog.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+export const saveBlog = jest.fn(() => {
+    return Promise.resolve()
+})
+
+export const updateBlog =jest.fn(() => {
+    return Promise.resolve()
+})
+
+export const getBlog = jest.fn(() => {
+    return Promise.resolve()
+})
+
+export const deleteBlog = jest.fn(() => {
+    return Promise.resolve()
+})
+
+export const getBlogs = jest.fn(() => {
+    return Promise.resolve()
+})
+
+export const getUserBlogs = jest.fn(() => {
+    return Promise.resolve()
+})
+
+export const getUserBlog = jest.fn(() => {
+    return Promise.resolve()
+})
--- resources/js/store/modules/__tests__/blog.spec.js
+++ resources/js/store/modules/__tests__/blog.spec.js
@@ -1,126 +1,102 @@
-const mockToken = 'xxx-xxx';
-const mockUser = {name: 'foo', email: 'foo@bar.com'}
-let mockError = false
-
-jest.mock('@/plugins/vue-authenticator', () => ({
-    getVueAuthenticate: jest.fn(() => {
-        return {
-            authenticate() {
-                return new Promise((resolve, reject) => {
-                    if (mockError)
-                        reject(new Error('API Error occurred.'))
-
-                    resolve({data: {token: mockToken, user: mockUser}})
-                })
-            }
-        }
-    })
-}))
-
-import { getters, mutations, actions } from '@/store/modules/auth'
+import { getters, mutations, actions } from '@/store/modules/blog'
 import flushPromises from 'flush-promises'
-import { sendPasswordResetLink, resetPassword } from '@/api/auth'
+import { sendPasswordResetLink, resetPassword } from '@/api/blog'
 
-jest.mock('@/api/auth')
+jest.mock('@/api/blog')
 
 
-describe('auth store module', () => {
+describe('blog store module', () => {
     let state
 
     beforeEach(() => {
         state = {
-            isAuthenticated: false,
-            user: null,
-            token: null
+            blogs: [],
+            blog: [],
         }
     })
 
     describe('getters', () => {
-        it('isAuthenticated logic works', () => {
-            expect(getters.isAuthenticated(state)).toBe(false)
-            state.isAuthenticated = true
-            expect(getters.isAuthenticated(state)).toBe(true)
+        it('get blogs', () => {
+            expect(getters.blogs(state)).toEqual([])
+            state.blogs = [{},{}]
+            expect(getters.blogs(state)).toEqual([{},{}])
         })
-        it('can get token', () => {
-            expect(getters.getToken(state)).toBe(null)
-            state.token = 'xxx-xxx-xxx'
-            expect(getters.getToken(state)).toBe('xxx-xxx-xxx')
-        })
-        it('can get auth user', () => {
-            expect(getters.authUser(state)).toBe(null)
-            state.user = {name: 'foo'}
-            expect(getters.authUser(state)).toEqual({name: 'foo'})
+
+        it('get blog', () => {
+            expect(getters.blog(state)).toEqual([])
+            state.blog = [{},{}]
+            expect(getters.blog(state)).toEqual([{},{}])
         })
     })
 
-    describe('mutations', () => {
-        it('set is authenticated', () => {
-            mutations.IS_AUTHENTICATED(state, {isAuthenticated: true})
-            expect(state.isAuthenticated).toBe(true)
-        })
-        it('set token', () => {
-            mutations.SET_AUTH_TOKEN(state, 'xxx-xxx-xxx')
-            expect(state.token).toBe('xxx-xxx-xxx')
-        })
-        it('set authenticated user', () => {
-            const user ={name: 'foo', email: 'foo@bar.com'};
-            mutations.SET_AUTH_USER(state, user)
-            expect(state.user).toBe(user)
-        })
-    })
-
-    describe('actions', () => {
-        it('login using a social account', async () => {
-            const commit = jest.fn()
-
-            await actions.socialLogin({ commit }, 'github')
-
-            expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: true})
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', mockUser)
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', mockToken)
-        })
-
-        it('can login using email', async () => {
-            const commit = jest.fn()
-
-            await actions.login({ commit }, {email: 'foo@bar.com', password: '@secret123'})
-
-            expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: true})
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', 'mocked user object')
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', 'mocked token')
-        })
-
-        it('user can register using personal details', async () => {
-            const commit = jest.fn()
-
-            await actions.register({ commit }, {foo: 'bar'})
-
-            expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: true})
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', 'mocked user object')
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', 'mocked token')
-        })
-
-        it('can logout authenticated users', async () => {
-            const commit = jest.fn()
-            await actions.logout({ commit })
-
-            expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: false})
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', null)
-            expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', null)
-        })
-
-        it('user can send forget password email', async () => {
-            const commit = jest.fn()
-            await actions.sendPasswordResetLink({ commit }, {foo: 'bar'})
-
-            expect(sendPasswordResetLink.mock.calls).toHaveLength(1)
-        })
-
-        it('user can reset password', async () => {
-            const commit = jest.fn()
-            await actions.resetPassword({ commit }, {foo: 'bar'})
-
-            expect(resetPassword.mock.calls).toHaveLength(1)
-        })
-    })
+    // describe('mutations', () => {
+    //     it('set is blogenticated', () => {
+    //         mutations.IS_AUTHENTICATED(state, {isAuthenticated: true})
+    //         expect(state.isAuthenticated).toBe(true)
+    //     })
+    //     it('set token', () => {
+    //         mutations.SET_AUTH_TOKEN(state, 'xxx-xxx-xxx')
+    //         expect(state.token).toBe('xxx-xxx-xxx')
+    //     })
+    //     it('set blogenticated user', () => {
+    //         const user ={name: 'foo', email: 'foo@bar.com'};
+    //         mutations.SET_AUTH_USER(state, user)
+    //         expect(state.user).toBe(user)
+    //     })
+    // })
+    //
+    // describe('actions', () => {
+    //     it('login using a social account', async () => {
+    //         const commit = jest.fn()
+    //
+    //         await actions.socialLogin({ commit }, 'github')
+    //
+    //         expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: true})
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', mockUser)
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', mockToken)
+    //     })
+    //
+    //     it('can login using email', async () => {
+    //         const commit = jest.fn()
+    //
+    //         await actions.login({ commit }, {email: 'foo@bar.com', password: '@secret123'})
+    //
+    //         expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: true})
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', 'mocked user object')
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', 'mocked token')
+    //     })
+    //
+    //     it('user can register using personal details', async () => {
+    //         const commit = jest.fn()
+    //
+    //         await actions.register({ commit }, {foo: 'bar'})
+    //
+    //         expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: true})
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', 'mocked user object')
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', 'mocked token')
+    //     })
+    //
+    //     it('can logout blogenticated users', async () => {
+    //         const commit = jest.fn()
+    //         await actions.logout({ commit })
+    //
+    //         expect(commit).toHaveBeenCalledWith("IS_AUTHENTICATED", {isAuthenticated: false})
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_USER', null)
+    //         expect(commit).toHaveBeenCalledWith('SET_AUTH_TOKEN', null)
+    //     })
+    //
+    //     it('user can send forget password email', async () => {
+    //         const commit = jest.fn()
+    //         await actions.sendPasswordResetLink({ commit }, {foo: 'bar'})
+    //
+    //         expect(sendPasswordResetLink.mock.calls).toHaveLength(1)
+    //     })
+    //
+    //     it('user can reset password', async () => {
+    //         const commit = jest.fn()
+    //         await actions.resetPassword({ commit }, {foo: 'bar'})
+    //
+    //         expect(resetPassword.mock.calls).toHaveLength(1)
+    //     })
+    // })
 })

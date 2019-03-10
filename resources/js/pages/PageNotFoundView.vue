f4c2233566b770e9ab39996b0056cef6639ba30d
michael ntow <oseintow@gmail.com>
2019-03-10 20:53:37 +0000

    ch(): added page not found view

--- resources/js/pages/PageNotFoundView.vue
+++ resources/js/pages/PageNotFoundView.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        Page not found
+    <div class="container h-100 d-flex justify-content-center">
+        <div class="my-auto">
+           <h5 class="display-3 not-found">Page Not Found</h5>
+        </div>
     </div>
 </template>
 
 <script>
     export default {}
-</script>    
+</script>
+
+<style>
+    .not-found{
+        margin-top: 30%
+    }
+</style>
